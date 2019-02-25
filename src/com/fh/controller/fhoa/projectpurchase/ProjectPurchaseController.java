b55df5207f98d07e3038e1e1c9724fd7c714ffe4
Robotx <13022000300@163.com>
2019-02-25 12:18:26 +0800

    修改BUG

--- src/com/fh/controller/fhoa/projectpurchase/ProjectPurchaseController.java
+++ src/com/fh/controller/fhoa/projectpurchase/ProjectPurchaseController.java
@@ -184,7 +184,7 @@ public class ProjectPurchaseController extends AcStartController {
         try {
             pd = this.getPageData();
             pd.put("STATUS", 1);
-            page.setShowCount(999999);
+            page.setShowCount(9999999);
             page.setPd(pd);
             list = projectpurchaseService.list(page);
         } catch (Exception e) {
